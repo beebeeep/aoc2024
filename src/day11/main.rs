ef7de15237d3ab01c6cf36046f6a49db2abdae2d
Danila Migalin <me@miga.me.uk>
2024-12-11 14:58:09 +0200

    optimize

--- src/day11/main.rs
+++ src/day11/main.rs
@@ -10,8 +10,8 @@ fn load_input(file: &str) -> Vec<i64> {
 
 fn multicount(mut stones: Vec<i64>, times: usize) -> usize {
     let mut handles: Vec<_> = Vec::new();
-    stones = blink(&stones);
-    stones = blink(&stones);
+    blink(&mut stones);
+    blink(&mut stones);
     for s in stones {
         let h = thread::spawn(move || count_stones(vec![s], times - 2, 0));
         handles.push(h);
@@ -25,7 +25,7 @@ fn multicount(mut stones: Vec<i64>, times: usize) -> usize {
 fn count_stones(mut stones: Vec<i64>, times: usize, depth: usize) -> usize {
     println!("depth {depth} len {}, times {times}", stones.len());
     for i in 0..times {
-        stones = blink(&stones);
+        blink(&mut stones);
         let l = stones.len();
         if l > 5000000 {
             return count_stones(stones[..l / 2].into(), times - i - 1, depth + 1)
@@ -35,27 +35,28 @@ fn count_stones(mut stones: Vec<i64>, times: usize, depth: usize) -> usize {
     return stones.len();
 }
 
-fn blink(stones: &[i64]) -> Vec<i64> {
-    let mut new_stones: Vec<i64> = Vec::new();
-    for stone in stones {
-        if *stone == 0 {
-            new_stones.push(1);
+fn blink(stones: &mut Vec<i64>) {
+    let l = stones.len();
+    for i in 0..l {
+        if stones[i] == 0 {
+            stones[i] = 1;
             continue;
         }
-        let pow = stone.ilog10() + 1;
+        let pow = stones[i].ilog10() + 1;
         if pow % 2 == 0 {
             let d = 10i64.pow(pow / 2);
-            new_stones.push(stone / d);
-            new_stones.push(stone % d);
+            let v = stones[i];
+            stones[i] = v / d;
+            stones.push(v % d);
             continue;
         }
-        new_stones.push(stone * 2024);
+        stones[i] *= 2024;
     }
-    return new_stones;
 }
 
 fn main() {
     let stones = load_input("src/day11/input.txt");
-    println!("part1: {}", multicount(stones.clone(), 25));
+
+    println!("part1: {}", multicount(stones.clone(), 20));
     println!("part2: {}", multicount(stones.clone(), 75));
 }
