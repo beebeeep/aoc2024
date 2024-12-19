7453be8164e04dcc871ec6e64171029e00486c43
Danila Migalin <me@miga.me.uk>
2024-12-19 16:51:46 +0200

    day 19 done

--- src/day19/main.rs
+++ src/day19/main.rs
@@ -1,3 +1,11 @@
+use core::time;
+use std::{
+    collections::{HashMap, HashSet},
+    hash::Hash,
+    sync::{Arc, RwLock},
+    thread,
+};
+
 use rayon::prelude::*;
 
 fn load_input(txt: &str) -> (Vec<String>, Vec<String>) {
@@ -31,16 +39,24 @@ fn is_possible(patterns: &Vec<String>, design: &str) -> bool {
     return false;
 }
 
-fn count_possible(patterns: &Vec<String>, design: &str) -> usize {
+fn count_possible(
+    patterns: &Vec<String>,
+    design: &str,
+    possibles: &mut Arc<RwLock<HashMap<String, usize>>>,
+) -> usize {
     if design.len() == 0 {
         return 1;
+    }
+    if let Some(count) = possibles.read().unwrap().get(design) {
+        return *count;
     }
     let mut count = 0;
     for pattern in patterns {
         if design.starts_with(pattern) {
-            count += count_possible(patterns, &design[pattern.len()..]);
+            count += count_possible(patterns, &design[pattern.len()..], possibles);
         }
     }
+    possibles.write().unwrap().insert(design.to_string(), count);
     return count;
 }
 
@@ -53,10 +69,13 @@ fn main() {
         .collect();
     println!("part1: {}", possible.len());
 
+    let cache: Arc<RwLock<HashMap<String, usize>>> = Arc::new(RwLock::new(HashMap::new()));
     let total_possibles: usize = possible
         .into_par_iter()
-        .map(|x| (x.clone(), count_possible(&patterns, &x)))
-        .inspect(|(design, count)| println!("for {design}: {count}"))
+        .map(|x| {
+            let mut cache = Arc::clone(&cache);
+            (x.clone(), count_possible(&patterns, &x, &mut cache))
+        })
         .fold(|| 0usize, |acc, (_, count)| acc + count)
         .sum::<usize>();
     println!("part2: {total_possibles}");
